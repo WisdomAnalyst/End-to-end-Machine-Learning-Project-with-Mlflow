90b5f9f881fe7c6ed4f44cde271ee4d3e7a407c0
Akano Wisdom <wisdomstrategictrade@gmail.com>
2024-11-21 18:27:28 -0800

    Installed requirements and set up project

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.1"
+
+REPO_NAME = "End-to-end-Machine-Learning-Project-with-Mlflow"
+AUTHOR_USER_NAME = "WisdomAnalyst"
+SRC_REPO = "Mlproject"
+AUTHOR_EMAIL = "wisdomstrategictrade@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A short description of your project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
