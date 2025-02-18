370564cb10c2c276b82d1a10982969ae21469cac
Akano Wisdom <wisdomstrategictrade@gmail.com>
2025-02-18 00:07:59 -0800

    model_trainer complete

--- README.md
+++ README.md
@@ -15,3 +15,71 @@
 
 
 
+# How to run ? 
+
+### STEPS
+
+Clone the repository
+
+
+***bash
+
+https://github.com/WisdomAnalyst/End-to-end-Machine-Learning-Project-with-Mlflow
+...
+
+### STEP 01- Create a conda environment after opening the repository
+
+***bash
+conda create -n mlproj python=3.8 -y
+***
+
+
+***bash
+conda activate mlproj
+***
+
+### STEP 2 - install the requirements 
+***bash
+pip install -r requirements.txt
+***
+
+
+***bash
+#finally run the following command 
+python app.py
+
+
+now, 
+***bash
+open up your local host and port
+
+
+#MLflow
+[Documentation](https://mlflow.org/docs/latest/index.html)
+
+
+
+#### cmd
+-mlflow ui
+
+
+
+### dagshub
+[dagshub](https;//dagshub.com/)
+
+
+MLFLOW_TRACKING_USERNAME=WisdomAnalyst
+MLFLOW_TRACKING_PASSWORD=781aa8b98086477f57088d72da8f137a4686f662
+MLFLOW_TRACKING_URI=https://dagshub.com/WisdomAnalyst/End-to-end-Machine-Learning-Project-with-Mlflow.mlflow
+python script.py
+
+
+Run this to export as env variables 
+
+***bash
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/WisdomAnalyst/End-to-end-Machine-Learning-Project-with-Mlflow.mlflow
+
+export MLFLOW_TRACKING_USERNAME=WisdomAnalyst
+
+export MLFLOW_TRACKING_PASSWORD=781aa8b98086477f57088d72da8f137a4686f662
--- src/Mlproject/config/configuration.py
+++ src/Mlproject/config/configuration.py
@@ -1,6 +1,6 @@
 from Mlproject.entity.config_entity import *
 from Mlproject.utils.common import read_yaml, create_directories
-from Mlproject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
+from Mlproject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig
 import logging
 from Mlproject.constants import *
 
@@ -105,3 +105,24 @@ class ConfigurationManager:
         )
         
         return model_trainer_config 
+    
+
+
+    def get_model_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_Evaluation
+        params = self.params.Elasticnet
+        schema = self.schema.TARGET_COLUMNS
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path=config.model_path,
+            all_params=params,
+            metric_file_name=config.metric_file_name,
+            target_column=schema.name,
+            mlflow_url='https://dagshub.com/WisdomAnalyst/End-to-end-Machine-Learning-Project-with-Mlflow.mlflow'
+        )
+
+        return model_evaluation_config
